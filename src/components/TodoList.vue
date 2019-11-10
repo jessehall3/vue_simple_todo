95b009ae9ad41bc140f58ff169969368aeac3cec
Jesse Hall <jessehall3@gmail.com>
2019-11-10 13:37:08 -0600

    Can edit todo content.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,18 +1,18 @@
 <template>
 	<div>
-		<BaseInputText 
+		<BaseInputText
 			v-model="newTodoText"
 			placeholder="New todo"
 			@keydown.enter="addTodo"
 		/>
-		<ul v-if="todos.length">
+		<ol v-if="todos.length">
 			<TodoListItem
 				v-for="todo in todos"
 				:key="todo.id"
 				:todo="todo"
 				@remove="removeTodo"
 			/>
-		</ul>
+		</ol>
 		<p v-else>
 			Nothing left in the list. Add a new todo in the input above.
 		</p>
@@ -35,16 +35,12 @@ export default {
       todos: [
 				{
 					id: nextTodoId++,
-					text: 'Learn Vue'
+					text: 'Buy tomotatoes.'
 				},
 				{
 					id: nextTodoId++,
-					text: 'Learn about single-file components'
+					text: 'Smell the roses.'
 				},
-				{
-					id: nextTodoId++,
-					text: 'Fall in love'
-				}
 			]
     }
   },
--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -1,19 +1,53 @@
 <template>
+  <div>
   <li>
-    {{ todo.text }}
+    <div v-if="this.editable">
+      <BaseInputText
+        v-if="this.editable"
+        v-model="updatedTodoText"
+        @keydown.enter="updateTodo"
+      />
+    </div>
+    <div v-else>
+      {{ todo.text }}
+    </div>
+    <button v-on:click="editTodo">
+      edit
+    </button>
     <button @click="$emit('remove', todo.id)">
-      X
+      delete
     </button>
   </li>
+</div>
 </template>
 
 <script>
+import BaseInputText from './BaseInputText.vue'
+
 export default {
+  components: {
+    BaseInputText
+  },
+  data () {
+    return {
+      updatedTodoText: this.todo.text,
+      editable: false,
+    }
+  },
   props: {
     todo: {
       type: Object,
       required: true
     }
+  },
+  methods: {
+    editTodo () {
+      this.editable = !this.editable
+    },
+    updateTodo () {
+			this.todo.text = this.updatedTodoText
+      this.editable = false
+		},
   }
 }
 </script>
